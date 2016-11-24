5c16840f09f7ca9136599a69b150f312d83fb6cc
haker20154 <behrang.parsa94@gmail.com>
2016-11-24 16:44:08 +0330

    Update sudo.lua

--- plugins/sudo.lua
+++ plugins/sudo.lua
@@ -1,6 +1,6 @@
 do
 local function run(msg, matches)
-local sudo = 85831686
+local sudo = 211752618
     if matches[1]:lower() == "insudo" then
        channel_invite_user("chat#id"..msg.to.id, 'user#id'..sudo, ok_cb, false)
     end
